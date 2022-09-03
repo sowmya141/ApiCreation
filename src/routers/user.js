43b81df4a66c29fa203bdef9d571107c967ee05b
sowmya.ks@goavega.com-at-012114828737 <sowmya.ks@goavega.com>
2022-09-03 16:33:41 +0530

    user Api working as expected"

--- src/routers/user.js
+++ src/routers/user.js
@@ -28,36 +28,9 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.post("/users/logout", auth, async (req, res) => {
-  try {
-    req.user.tokens = req.user.tokens.filter((token) => {
-      return token.token !== req.token;
-    });
-    await req.user.save();
-
-    res.send();
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-router.post("/users/logoutAll", auth, async (req, res) => {
-  try {
-    req.user.tokens = [];
-    await req.user.save();
-    res.send();
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-router.get("/users/me", auth, async (req, res) => {
-  res.send(req.user);
-});
-
 router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "password", "age"];
+  const allowedUpdates = ["addressline", "city", "state", "zip"];
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update)
   );
@@ -72,15 +45,6 @@ router.patch("/users/me", auth, async (req, res) => {
     res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
-  }
-});
-
-router.delete("/users/me", auth, async (req, res) => {
-  try {
-    await req.user.remove();
-    res.send(req.user);
-  } catch (e) {
-    res.status(500).send();
   }
 });
 
